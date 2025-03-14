ede4cd4ac70d909e50fd157714ab3ea99c3547d3
csdarren <2001dhall@gmail.com>
2025-03-13 22:26:57 -0400

    First Commit

---
+++ src/Master.h
@@ -0,0 +1,13 @@
+#ifndef MASTER_H
+#define MASTER_H
+#include "raylib.h"
+#include "raymath.h"
+#include "imgui.h"
+#include "rlImGui.h"
+
+#include "InputHandler.hpp"
+
+#include <vector>
+#include <iostream>
+
+#endif
